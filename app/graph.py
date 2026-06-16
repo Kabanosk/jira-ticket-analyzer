0525ed4328855ef0c4090105cb04641506540cb2
Wojciech Fiolka <wojciech.fiolka@telit.com>
2026-06-16 22:50:59 +0200

    add nodes and graph definition

---
+++ app/graph.py
@@ -0,0 +1,21 @@
+from langgraph.graph import StateGraph, START, END
+
+from app.state import TicketState
+from app.nodes import analyze_node, classify_node, recommend_node
+from app.database import checkpointer
+
+
+graph = StateGraph(TicketState)
+
+graph.add_node("classify", classify_node)
+graph.add_node("analyze", analyze_node)
+graph.add_node("recommend", recommend_node)
+
+graph.add_edge(START, "classify")
+graph.add_edge("classify", "analyze")
+graph.add_edge("analyze", "recommend")
+graph.add_edge("recommend", END)
+
+def create_graph():
+    compiled_graph = graph.compile(checkpointer=checkpointer)
+    return compiled_graph
